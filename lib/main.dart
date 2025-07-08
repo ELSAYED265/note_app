160aaa6d861bbfbe84fcdee16013cdbf7f41e1a4
ELSAYED265 <arrr9990@gmail.com>
2025-07-08 15:09:05 +0300

    note_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/notes_view.dart';
 
 void main() {
   runApp(NotesApp());
@@ -8,6 +9,6 @@ class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(home: NotesView(), theme: ThemeData.dark());
   }
 }

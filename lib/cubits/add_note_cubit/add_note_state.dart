6a4b05337e7aea7bd7596ca2594a164500514c23
ELSAYED265 <arrr9990@gmail.com>
2025-07-10 15:22:03 +0300

    create note model adaptor

--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -4,3 +4,12 @@ part of 'add_note_cubit.dart';
 sealed class AddNoteState {}
 
 final class AddNoteInitial extends AddNoteState {}
+
+class AddnoteLoaded extends AddNoteState {}
+
+class AddnoteSuccess extends AddNoteState {}
+
+class AddnoteFailer extends AddNoteState {
+  final String errMassage;
+  AddnoteFailer(this.errMassage);
+}
